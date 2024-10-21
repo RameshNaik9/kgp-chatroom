309e5dcf741ca654415154deccc7de164ec42dc2
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-10-21 08:50:10 +0530

    comm

--- client/src/components/ChatComponent.js
+++ client/src/components/ChatComponent.js
@@ -381,7 +381,7 @@ const ChatroomComponent = ({ onProfileClick }) => {  // Pass function to parent
                 })}
                 <div ref={messagesEndRef} />
             </div>
-            <form onSubmit={handleSendMessage} className="p-3 border-top">
+            <form onSubmit={handleSendMessage} className="p-1 mx-5 border-top">
                 {replyToMessage && (
                     <div className={`reply-to-container text-muted small p-2 rounded border ${theme === "dark" ? "text-light" : "text-dark"}`}>
                         <span>
