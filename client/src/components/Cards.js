bdadcd568e44a17cc4a018d5db39f3384aba2c21
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-10-21 16:38:24 +0530

    mesage bar conversation comp

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -5,14 +5,13 @@ const Cards = () => {
         "What career options do I have?",
         "How do I prepare for interviews?",
         "Can you help with resume building?",
-        "Tell me more about the top companies."
     ];
 
     return (
         <div className="cards">
             {recommendations.map((question, index) => (
                 <div key={index} className="card1">
-                    <p>{question}</p>
+                    {question}
                 </div>
             ))}
         </div>
