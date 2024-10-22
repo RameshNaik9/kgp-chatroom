53807a50705e25fa2a54f0aae22fefd5839539e0
azmerachandu <138515161+azmerachandu@users.noreply.github.com>
2024-10-22 15:49:50 +0530

    home page changes

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,83 +1,56 @@
-import React from "react";
-import './HomeComponent.css'; // Import the CSS for the component
-
+import React from 'react';
+import './HomeComponent.css';
+import img1 from '../media/Academics.svg';
+import img2 from '../media/Career.svg';
+import img3 from '../media/Gymkhana.svg';
+import img4 from '../media/placements.jpeg';
 const HomeComponent = () => {
   return (
-    <div>
-      <a
-        className="bear-link"
-        href="https://twitter.com/intent/follow?screen_name=jh3yy"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <svg
-          className="w-9"
-          viewBox="0 0 969 955"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-            cx="161.191"
-            cy="320.191"
-            r="133.191"
-            stroke="currentColor"
-            strokeWidth="20"
-          ></circle>
-          <circle
-            cx="806.809"
-            cy="320.191"
-            r="133.191"
-            stroke="currentColor"
-            strokeWidth="20"
-          ></circle>
-          <circle
-            cx="695.019"
-            cy="587.733"
-            r="31.4016"
-            fill="currentColor"
-          ></circle>
-          <circle
-            cx="272.981"
-            cy="587.733"
-            r="31.4016"
-            fill="currentColor"
-          ></circle>
-          <path
-            d="M564.388 712.083C564.388 743.994 526.035 779.911 483.372 779.911C440.709 779.911 402.356 743.994 402.356 712.083C402.356 680.173 440.709 664.353 483.372 664.353C526.035 664.353 564.388 680.173 564.388 712.083Z"
-            fill="currentColor"
-          ></path>
-          <rect
-            x="310.42"
-            y="448.31"
-            width="343.468"
-            height="51.4986"
-            fill="#FF1E1E"
-          ></rect>
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M745.643 288.24C815.368 344.185 854.539 432.623 854.539 511.741H614.938V454.652C614.938 433.113 597.477 415.652 575.938 415.652H388.37C366.831 415.652 349.37 433.113 349.37 454.652V511.741L110.949 511.741C110.949 432.623 150.12 344.185 219.845 288.24C289.57 232.295 384.138 200.865 482.744 200.865C581.35 200.865 675.918 232.295 745.643 288.24Z"
-            fill="currentColor"
-          ></path>
-        </svg>
-      </a>
-      <nav></nav>
+    <div className="containerk">
       <main>
-        {Array.from({ length: 10 }, (_, index) => (
-          <article key={index} style={{ "--index": index }}>
-            <div className="article__img">
-              <img
-                src={`https://picsum.photos/1920/1080?random=${index + 1}`}
-                alt=""
-              />
-            </div>
-            <div className="article__info">
-              <h2>{`Slide ${index + 1}`}</h2>
-              <p>{`This is content for slide ${index + 1}`}</p>
-              <a href="#">Action</a>
-            </div>
-          </article>
-        ))}
+        <article style={{ "--index": 0 }}>
+          <div className="article__img">
+            <img src={img1} alt="" style={{borderRadius:'0px'}}/>
+          </div>
+          <div className="article__info">
+            <h2>CSS Scroll Animations</h2>
+            <p>Check out this rad demo</p>
+            <a href="#">Follow</a>
+          </div>
+        </article>
+
+        <article style={{ "--index": 1 }}>
+          <div className="article__img">
+            <img src={img2} alt="" style={{borderRadius:'0px'}}/>
+          </div>
+          <div className="article__info">
+            <h2>Animate on scroll</h2>
+            <p>Works with media queries too</p>
+            <a href="#">Check it out</a>
+          </div>
+        </article>
+
+        <article style={{ "--index": 2 }}>
+          <div className="article__img">
+            <img src={img3} alt="" style={{borderRadius:'0px'}}/>
+          </div>
+          <div className="article__info">
+            <h2>Parallax Effects</h2>
+            <p>Tweak your timings</p>
+            <a href="#">Get Styling</a>
+          </div>
+        </article>
+
+        <article style={{ "--index": 3 }}>
+          <div className="article__img">
+            <img src={img4} alt="" style={{borderRadius:'0px'}} />
+          </div>
+          <div className="article__info">
+            <h2>Drive Keyframes</h2>
+            <p>CSS alone and...</p>
+            <a href="#">No JavaScript</a>
+          </div>
+        </article>
       </main>
     </div>
   );
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,13 +3,9 @@ import Header from '../components/Header';
 import ChatDrawer from '../components/ChatDrawer';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import img1 from '../media/robott.png';
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
-import HomeComponent from '../components/HomeComponent';
+import HomeComponent from '../components/HomeComponent.js';
 
 
 const HomePage = () => {
@@ -19,44 +15,14 @@ const HomePage = () => {
     const toggleDrawer = () => {
         setDrawerOpen(!drawerOpen);
     };
-
-    const products = [
-        { name: "General Chat", description: "Engage in lively discussions with your peers!", route: "/general-chat", backInfo: "Chat with Kgpians across various topics." },
-        { name: "Career Assistant", description: "Get guidance on career planning and job searches.", route: "/career-assistant", backInfo: "Clarify your CDC related doubts and prepare for interviews." },
-        { name: "Acads Assistant", description: "Find resources and help for your academic queries.", route: "/acads-assistant", backInfo: "Support for your coursework and research." },
-        { name: "General Assistant", description: "Assistance with general inquiries and more.", route: "/general-assistant", backInfo: "Help with day-to-day inquiries and more." }
-    ];
-
     const handleNavigation = (route) => {
         navigate(route);
     };
 
     return (
-        <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
-            <Header toggleDrawer={toggleDrawer} />
-            <div style={{ display: 'flex', flexGrow: 1 }}>
-                <Drawer
-                    anchor="left"
-                    open={drawerOpen}
-                    onClose={toggleDrawer}
-                    variant="persistent"
-                    sx={{
-                        width: 250,
-                        flexShrink: 0,
-                        '& .MuiDrawer-paper': {
-                            width: 250,
-                            top: '67px',
-                            boxSizing: 'border-box',
-                        },
-                    }}
-                >
-                    <ChatDrawer />
-                </Drawer>
-                <Box >
-                    <HomeComponent/>
-                </Box>
-            </div>
-        </div>
+        <> 
+           <HomeComponent/>
+        </>
     );
 };
 
