e93a7e9ee28b19cfc677bf577b3fc19584cefaab
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-12-14 16:20:35 +0530

    images

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -4,23 +4,23 @@ import './HomeComponent.css';
 
 
 const HomeComponent = () => {
-  const img1 = '/media/Academics.svg';
+  const img1 = '/media/Chatroom.svg';
   const img2 = '/media/Career.svg';
   const img3 = '/media/Academics.svg';
   const img4 = '/media/Gymkhana.svg';
-
+  const img5 = '/media/Bhaat.svg';
 
   return (
     <div className="containerk">
       <main>
         <article style={{ "--index": 0 }}>
           <div className="article__img">
-            <img src={img1} alt="Academics" style={{borderRadius:'0px'}} />
+            <img src={img1} alt="Chatroom" style={{borderRadius:'0px'}} />
           </div>
           <div className="article__info">
             <h2 className="smoke">Engage in lively discussions with your peers</h2>
             <Link to="/group-chat">
-              <button className="glow-on-hover">Kgp Chatroom</button>
+              <button className="glow-on-hover">KGP Chatroom</button>
             </Link>
           </div>
         </article>
@@ -30,7 +30,7 @@ const HomeComponent = () => {
             <img src={img2} alt="Career" style={{borderRadius:'0px'}} />
           </div>
           <div className="article__info">
-            <h2 className="smoke">Get guidance on career planning and job searches</h2>
+            <h2 className="smoke">Get fundae on internships,placements and career planning</h2>
             <Link to="/career-assistant">
               <button className="glow-on-hover" type="button">Career Assistant</button>
             </Link>
@@ -39,7 +39,7 @@ const HomeComponent = () => {
 
         <article style={{ "--index": 2 }}>
           <div className="article__img">
-            <img src={img3} alt="Gymkhana" style={{borderRadius:'0px'}} />
+            <img src={img3} alt="Academics" style={{borderRadius:'0px'}} />
           </div>
           <div className="article__info">
             <h2 className="smoke">Find resources and help for your academic queries</h2>
@@ -54,8 +54,8 @@ const HomeComponent = () => {
             <img src={img4} alt="Placements" style={{borderRadius:'0px'}} />
           </div>
           <div className="article__info">
-            <h2 className="smoke">Assistance with Gymkhana related information</h2>
-            <Link to="/general-assistant">
+            <h2 className="smoke"> PoRs, Fests, GC and much more </h2>
+            <Link to="/gymkhana-assistant">
               <button className="glow-on-hover">Gymkhana Assistant</button>
             </Link>
           </div>
@@ -63,11 +63,11 @@ const HomeComponent = () => {
 
                 <article style={{ "--index": 4 }}>
           <div className="article__img">
-            <img src={img4} alt="Placements" style={{borderRadius:'0px'}} />
+            <img src={img5} alt="Placements" style={{borderRadius:'0px'}} />
           </div>
           <div className="article__info">
-            <h2 className="smoke">Assistance with general inquiries and more...</h2>
-            <Link to="/general-assistant">
+            <h2 className="smoke">Miscellaneous</h2>
+            <Link to="/bhaat-assistant">
               <button className="glow-on-hover">Bhaat Assistant</button>
             </Link>
           </div>
